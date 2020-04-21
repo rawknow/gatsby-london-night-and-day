2fbd03731dbd5cdb2781c75d34a07f4d68c817e6
rawknow <ronoyang@gmail.com>
2020-04-21 15:36:23 -0400

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: 'RONALD YANG',
   shortName: '',
-  description: '35mm/120mm film photography, and digital sometimes.',
+  description: '35mm/120mm film photography, sometimes digital.',
   url: 'https://nervous-perlman-c6dd4e.netlify.app', // Domain of your site without prefix!
   prefix: '/',
   author: 'ronald',
