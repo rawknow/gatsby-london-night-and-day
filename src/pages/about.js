4f95b7db48d5afe794aff63516f1afecaa76f0ca
rawknow <ronoyang@gmail.com>
2020-04-22 16:47:37 -0400

    fix form

--- src/pages/about.js
+++ src/pages/about.js
@@ -48,11 +48,21 @@ const AboutPage = ({ data }, location) => {
             and I promise I'll probably read that email.
           </p>
         </div>
-        <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-  {/* You still need to add the hidden input with the form name to your JSX form */}
-  <input type="hidden" name="form-name" value="contact" />
-  ...
-</form>
+        <form name="contact" method="POST" data-netlify="true">
+          <p>
+             <label>Your Name: <input type="text" name="name" /></label>   
+          </p>
+          <p>
+             <label>Your Email: <input type="email" name="email" /></label>
+          </p>
+          <p>
+            <label>Message: <textarea name="message"></textarea></label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+
 
       </article>
     </Layout>
